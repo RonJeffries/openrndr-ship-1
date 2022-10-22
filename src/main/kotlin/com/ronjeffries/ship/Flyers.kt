ba9d8312b54891d50bb92cb105cd8570413776fc
Ron Jeffries <ronjeffries@acm.org>
2022-10-22 09:23:37 -0400

    remove import

--- src/main/kotlin/com/ronjeffries/ship/Flyers.kt
+++ src/main/kotlin/com/ronjeffries/ship/Flyers.kt
@@ -7,16 +7,37 @@ class Flyers {
         flyers.add(flyer)
     }
 
+//    fun colliders(): Set<FlyingObject> {
+//        val colliding = mutableSetOf<FlyingObject>()
+//        var ct = 0
+//        for (f1 in flyers ){
+//            for (f2 in flyers) {
+//                ct += 1
+//                if (f1.collides(f2)) {
+//                    colliding.add(f1)
+//                    colliding.add(f2)
+//                }
+//            }
+//        }
+//        print(ct)
+//        return colliding
+//    }
+
     fun colliders(): Set<FlyingObject> {
         val colliding = mutableSetOf<FlyingObject>()
-        for (f1 in flyers ){
-            for (f2 in flyers) {
+        var ct = 0
+        for (i in 0 until flyers.size-1){
+            val f1 = flyers[i]
+            for (j in i until flyers.size) {
+                val f2 = flyers[j]
+                ct += 1
                 if (f1.collides(f2)) {
                     colliding.add(f1)
                     colliding.add(f2)
                 }
             }
         }
+        print(ct)
         return colliding
     }
 
--- src/test/kotlin/com/ronjeffries/ship/FlyersTest.kt
+++ src/test/kotlin/com/ronjeffries/ship/FlyersTest.kt
@@ -3,7 +3,6 @@ package com.ronjeffries.ship
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.openrndr.math.Vector2
-import java.lang.Math.*
 
 class FlyersTest {
     @Test
@@ -26,5 +25,25 @@ class FlyersTest {
         assertThat(flyers.size).isEqualTo(2)
         val colliders = flyers.colliders()
         assertThat(colliders.size).isEqualTo(2)
+    }
+
+    @Test
+    fun `stringent colliders`() {
+        val p1 = Vector2(100.0,100.0)
+        val p2 = Vector2(500.0, 500.0)
+        val flyers = Flyers()
+        val v = Vector2.ZERO
+        val a0 = FlyingObject.asteroid(p1,v) // yes
+        flyers.add(a0)
+        val m1 = FlyingObject(p1, v, Vector2.ZERO, 10.0) // yes
+        flyers.add(m1)
+        val s2 = FlyingObject.ship(p1) // yes
+        flyers.add(s2)
+        val a3 = FlyingObject.asteroid(p2,v) // no
+        flyers.add(a3)
+        val a4 = FlyingObject.asteroid(p2,v) // no
+        flyers.add(a4)
+        val colliders = flyers.colliders()
+        assertThat(colliders.size).isEqualTo(3)
     }
 }
