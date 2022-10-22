9ec17353bb28fa9d08968da21ed74b34cbbe652f
Ron Jeffries <ronjeffries@acm.org>
2022-10-22 09:25:26 -0400

    remove info print

--- src/main/kotlin/com/ronjeffries/ship/Flyers.kt
+++ src/main/kotlin/com/ronjeffries/ship/Flyers.kt
@@ -25,19 +25,16 @@ class Flyers {
 
     fun colliders(): Set<FlyingObject> {
         val colliding = mutableSetOf<FlyingObject>()
-        var ct = 0
         for (i in 0 until flyers.size-1){
             val f1 = flyers[i]
             for (j in i until flyers.size) {
                 val f2 = flyers[j]
-                ct += 1
                 if (f1.collides(f2)) {
                     colliding.add(f1)
                     colliding.add(f2)
                 }
             }
         }
-        print(ct)
         return colliding
     }
 
