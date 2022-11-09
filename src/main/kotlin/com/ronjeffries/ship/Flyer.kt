8b161ec277d71e06a2ab47ab3add107aae91254c
Ron Jeffries <ronjeffries@acm.org>
2022-11-08 20:31:25 -0500

    new constants

--- src/main/kotlin/com/ronjeffries/ship/Flyer.kt
+++ src/main/kotlin/com/ronjeffries/ship/Flyer.kt
@@ -3,14 +3,20 @@ package com.ronjeffries.ship
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
 import org.openrndr.extra.color.presets.MEDIUM_SLATE_BLUE
+import org.openrndr.extra.noise.random
 import org.openrndr.math.Vector2
-
-const val SPEED_OF_LIGHT = 5000.0
-const val UNIVERSE_SIZE = 10000.0
 
 typealias Point = Vector2
 typealias Velocity = Vector2
 typealias Acceleration = Vector2
+
+object U {
+    const val SPEED_OF_LIGHT = 5000.0
+    const val UNIVERSE_SIZE = 10000.0
+    val CENTER_OF_UNIVERSE = Point(UNIVERSE_SIZE/2, UNIVERSE_SIZE/2)
+    const val SAFE_SHIP_DISTANCE = UNIVERSE_SIZE/10.0
+    fun randomPoint() = Point(random(0.0, UNIVERSE_SIZE), random(0.0, UNIVERSE_SIZE))
+}
 
 fun Point.cap(): Point {
     return Point(this.x.cap(), this.y.cap())
@@ -18,12 +24,12 @@ fun Point.cap(): Point {
 
 fun Velocity.limitedToLightSpeed(): Velocity {
     val speed = this.length
-    return if (speed < SPEED_OF_LIGHT) this
-    else this*(SPEED_OF_LIGHT/speed)
+    return if (speed < U.SPEED_OF_LIGHT) this
+    else this*(U.SPEED_OF_LIGHT/speed)
 }
 
 fun Double.cap(): Double {
-    return (this + UNIVERSE_SIZE) % UNIVERSE_SIZE
+    return (this + U.UNIVERSE_SIZE) % U.UNIVERSE_SIZE
 }
 
 interface IFlyer {
@@ -156,7 +162,7 @@ class Flyer(
 
         fun missile(ship: Flyer): Flyer {
             val missileKillRadius = 10.0
-            val missileOwnVelocity = Velocity(SPEED_OF_LIGHT / 3.0, 0.0).rotate(ship.heading)
+            val missileOwnVelocity = Velocity(U.SPEED_OF_LIGHT / 3.0, 0.0).rotate(ship.heading)
             val standardOffset = Point(2 * (ship.killRadius + missileKillRadius), 0.0)
             val rotatedOffset = standardOffset.rotate(ship.heading)
             val missilePos: Point = ship.position + rotatedOffset
