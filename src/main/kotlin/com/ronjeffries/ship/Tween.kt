d95510c9312fa647c96832bb5270b1bd8cf9ba7c
Ron Jeffries <ronjeffries@acm.org>
2022-11-11 15:33:28 -0500

    tidying trailing comma

--- src/main/kotlin/com/ronjeffries/ship/Tween.kt
+++ src/main/kotlin/com/ronjeffries/ship/Tween.kt
@@ -5,7 +5,7 @@ import kotlin.math.max
 
 class Tween(private val start: Double, private val end: Double, private val duration: Double) {
     fun value(t:Double): Double {
-        val fraction = max(0.0, min(t/duration, 1.0,))
+        val fraction = max(0.0, min(t / duration, 1.0))
         return end*fraction + start*(1.0 - fraction)
     }
 }
