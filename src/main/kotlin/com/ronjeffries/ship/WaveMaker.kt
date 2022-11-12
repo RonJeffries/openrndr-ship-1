fc5e26ecbfd785e50305be33b3b0ca9e5460f287
Ron Jeffries <ronjeffries@acm.org>
2022-11-12 11:53:00 -0500

    WaveMaker works

--- src/main/kotlin/com/ronjeffries/ship/WaveMaker.kt
+++ src/main/kotlin/com/ronjeffries/ship/WaveMaker.kt
@@ -3,8 +3,11 @@ package com.ronjeffries.ship
 import kotlin.random.Random
 
 class WaveMaker(val numberToCreate: Int = 8): ISpaceObject {
+    override var elapsedTime = 0.0
+    var done = false
     override fun interactWith(other: ISpaceObject): List<ISpaceObject> {
-        return listOf(this)
+        return if ( done ) listOf(this)
+        else emptyList()
     }
 
     override fun interactWithOther(other: ISpaceObject): List<ISpaceObject> {
@@ -12,12 +15,16 @@ class WaveMaker(val numberToCreate: Int = 8): ISpaceObject {
     }
 
     override fun update(deltaTime: Double): List<ISpaceObject> {
+        elapsedTime += deltaTime
+        if (elapsedTime < 3) return emptyList()
+
         val list = mutableListOf<ISpaceObject>()
         for (i in 1..numberToCreate) {
             val vel = Velocity(1000.0, 0.0).rotate(Random.nextDouble(0.0, 360.0))
             val a = SolidObject.asteroid((U.randomEdgePoint()), vel)
             list.add(a)
         }
+        done = true
         return list
     }
 }
--- src/test/kotlin/com/ronjeffries/ship/WaveMakerTest.kt
+++ src/test/kotlin/com/ronjeffries/ship/WaveMakerTest.kt
@@ -8,16 +8,11 @@ class WaveMakerTest {
     fun `creates wave on update, removes self on interaction`() {
         val a = SolidObject.asteroid(U.randomPoint(), U.randomWelocity(1000.0))
         val wm = WaveMaker(7)
-        val toCreate = wm.update(0.01)
+        val toCreate = wm.update(3.01)
         assertThat(toCreate.size).isEqualTo(7)
         var toDestroy = wm.interactWithOther(wm)
         assertThat(toDestroy[0]).isEqualTo(wm)
         toDestroy = wm.interactWith(wm)
         assertThat(toDestroy[0]).isEqualTo(wm)
-    }
-
-    @Test
-    fun `counts asteroids`() {
-
     }
 }
