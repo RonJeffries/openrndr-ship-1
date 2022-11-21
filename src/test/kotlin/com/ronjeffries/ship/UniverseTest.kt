f8e7639b135c39430ce606d5386c1278f975abca
GeePaw Hill <geepawhill@geepawhill.org>
2022-11-20 23:03:03 -0500

    Brought SpaceObjectCollisionTest and UniverseTest fully back online.

--- src/test/kotlin/com/ronjeffries/ship/UniverseTest.kt
+++ src/test/kotlin/com/ronjeffries/ship/UniverseTest.kt
@@ -1,31 +1,22 @@
 package com.ronjeffries.ship
 
-import org.junit.jupiter.api.Disabled
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class UniverseTest {
 
-    @Disabled("Don't understand, forces SolidObject construction")
     @Test
     fun `collision calculation`() {
-//        val ship = SolidObject(
-//            Vector2.ZERO,
-//            Vector2.ZERO,
-//            100.0
-//        )
-//        val asteroid = SolidObject(
-//            position = Vector2.ZERO,
-//            velocity = Vector2.ZERO,
-//            killRadius = 1000.0
-//        )
-//        assertThat(ship.interactWith(asteroid).size).describedAs("on top").isEqualTo(2)
-//        val tooFar = Vector2(ship.killRadius + asteroid.killRadius + 1, 0.0)
-//        var rotated = tooFar.rotate(37.0)
-//        ship.position = rotated
-//        assertThat(ship.interactWith(asteroid).size).describedAs("too far").isEqualTo(0)
-//        val closeEnough = Vector2(ship.killRadius + asteroid.killRadius - 1, 0.0)
-//        rotated = closeEnough.rotate(37.0)
-//        ship.position = rotated
-//        assertThat(ship.interactWith(asteroid).size).describedAs("too close").isEqualTo(2)
+        val ship = Ship(Point.ZERO)
+        val asteroid = Asteroid(Point.ZERO, Point.ZERO, 1000.0)
+        assertThat(ship.interactions.interactWith(asteroid).size).describedAs("on top").isEqualTo(2)
+        val tooFar = Point(ship.killRadius + asteroid.killRadius + 1, 0.0)
+        var rotated = tooFar.rotate(37.0)
+        ship.position = rotated
+        assertThat(ship.interactions.interactWith(asteroid).size).describedAs("too far").isEqualTo(0)
+        val closeEnough = Point(ship.killRadius + asteroid.killRadius - 1, 0.0)
+        rotated = closeEnough.rotate(37.0)
+        ship.position = rotated
+        assertThat(ship.interactions.interactWith(asteroid).size).describedAs("too close").isEqualTo(2)
     }
 }
