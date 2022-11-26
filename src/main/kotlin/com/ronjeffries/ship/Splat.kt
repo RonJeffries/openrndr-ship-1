c93d139433685ed59fc4a204e88d52cd4ef69adf
GeePaw Hill <geepawhill@geepawhill.org>
2022-11-26 12:00:11 -0500

    Made Splat draw itself, killed SplatView.

--- src/main/kotlin/com/ronjeffries/ship/Splat.kt
+++ src/main/kotlin/com/ronjeffries/ship/Splat.kt
@@ -3,15 +3,19 @@ package com.ronjeffries.ship
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
 import org.openrndr.extra.color.presets.MEDIUM_SLATE_BLUE
+import kotlin.random.Random
 
 class Splat(var position: Point) : ISpaceObject, InteractingSpaceObject {
-    
+
     constructor(ship: Ship) : this(ship.position)
     constructor(missile: Missile) : this(missile.position)
 
     var elapsedTime = 0.0
     val lifetime = 2.0
-    var view = SplatView(2.0)
+
+    private val heading = Random.nextDouble(0.0, 360.0)
+    private val sizeTween = Tween(20.0, 100.0, lifetime)
+    private val radiusTween = Tween(30.0, 5.0, lifetime)
 
     override fun update(deltaTime: Double, trans: Transaction) {
         elapsedTime += deltaTime
@@ -21,7 +25,14 @@ class Splat(var position: Point) : ISpaceObject, InteractingSpaceObject {
     override fun draw(drawer: Drawer) {
         drawer.fill = ColorRGBa.MEDIUM_SLATE_BLUE
         drawer.translate(position)
-        view.draw(this, drawer)
+        drawer.stroke = ColorRGBa.RED
+        drawer.fill = ColorRGBa.RED
+        drawer.rotate(heading)
+        for (point in points) {
+            val size = sizeTween.value(elapsedTime)
+            val radius = radiusTween.value(elapsedTime)
+            drawer.circle(size * point.x, size * point.y, radius)
+        }
     }
 
     override fun finalize(): List<ISpaceObject> {
@@ -30,4 +41,20 @@ class Splat(var position: Point) : ISpaceObject, InteractingSpaceObject {
 
     override val interactions: Interactions = Interactions()
     override fun callOther(other: InteractingSpaceObject, trans: Transaction) {}
+
+    companion object {
+        val points = listOf(
+            Point(-2.0, 0.0),
+            Point(-2.0, -2.0),
+            Point(2.0, -2.0),
+            Point(3.0, 1.0),
+            Point(2.0, -1.0),
+            Point(0.0, 2.0),
+            Point(1.0, 3.0),
+            Point(-1.0, 3.0),
+            Point(-4.0, -1.0),
+            Point(-3.0, 1.0)
+        )
+
+    }
 }
