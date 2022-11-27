acf0f7d13cddd1945f11fbe985731a5e1529e6fb
GeePaw Hill <geepawhill@geepawhill.org>
2022-11-27 12:12:44 -0500

    Added some tests against TypedObjects to cover all types.

--- src/test/kotlin/com/ronjeffries/ship/TypedObjectsTest.kt
+++ src/test/kotlin/com/ronjeffries/ship/TypedObjectsTest.kt
@@ -8,13 +8,41 @@ class TypedObjectsTest {
     val objects = TypedObjects()
 
     @Test
-    fun `add splats works`() {
+    fun `add of each type works`() {
         val ship = Ship(Point.ZERO)
-        val splat1 = Splat(ship)
-        objects.add(splat1)
-        val splat2 = Splat(ship)
-        objects.add(splat2)
-        assertThat(objects.splats).containsExactlyInAnyOrder(splat1,splat2)
-        assertThat(objects.all).containsExactlyInAnyOrder(splat1,splat2)
+        objects.add(ship)
+        val splat = Splat(ship)
+        objects.add(splat)
+        val asteroid = Asteroid(U.randomPoint())
+        objects.add(asteroid)
+        val missile = Missile(ship)
+        objects.add(missile)
+        assertThat(objects.others).containsExactly(ship)
+        assertThat(objects.splats).containsExactly(splat)
+        assertThat(objects.missiles).containsExactly(missile)
+        assertThat(objects.asteroids).containsExactly(asteroid)
+        assertThat(objects.all).containsExactlyInAnyOrder(ship, splat, missile, asteroid)
+    }
+
+    @Test
+    fun `remove of each type works`() {
+        val ship = Ship(Point.ZERO)
+        objects.add(ship)
+        val splat = Splat(ship)
+        objects.add(splat)
+        val asteroid = Asteroid(U.randomPoint())
+        objects.add(asteroid)
+        val missile = Missile(ship)
+        objects.add(missile)
+
+        objects.remove(ship)
+        objects.remove(splat)
+        objects.remove(asteroid)
+        objects.remove(missile)
+        assertThat(objects.others).isEmpty()
+        assertThat(objects.splats).isEmpty()
+        assertThat(objects.missiles).isEmpty()
+        assertThat(objects.asteroids).isEmpty()
+        assertThat(objects.all).isEmpty()
     }
 }
