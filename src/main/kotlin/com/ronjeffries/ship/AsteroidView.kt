aa10c6033cd3f3b2540548858999ecda86077e6c
Ron Jeffries <ronjeffries@acm.org>
2022-11-30 09:58:04 -0500

    Tidy AsteroidView.

--- src/main/kotlin/com/ronjeffries/ship/AsteroidView.kt
+++ src/main/kotlin/com/ronjeffries/ship/AsteroidView.kt
@@ -3,8 +3,37 @@ package com.ronjeffries.ship
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
 
+private val rocks = listOf(
+    listOf(
+        Point(4.0, 2.0), Point(3.0, 0.0), Point(4.0, -2.0),
+        Point(1.0, -4.0), Point(-2.0, -4.0), Point(-4.0, -2.0),
+        Point(-4.0, 2.0), Point(-2.0, 4.0), Point(0.0, 2.0),
+        Point(2.0, 4.0), Point(4.0, 2.0),
+    ),
+    listOf(
+        Point(2.0, 1.0), Point(4.0, 2.0), Point(2.0, 4.0),
+        Point(0.0, 3.0), Point(-2.0, 4.0), Point(-4.0, 2.0),
+        Point(-3.0, 0.0), Point(-4.0, -2.0), Point(-2.0, -4.0),
+        Point(-1.0, -3.0), Point(2.0, -4.0), Point(4.0, -1.0),
+        Point(2.0, 1.0)
+    ),
+    listOf(
+        Point(-2.0, 0.0), Point(-4.0, -1.0), Point(-2.0, -4.0),
+        Point(0.0, -1.0), Point(0.0, -4.0), Point(2.0, -4.0),
+        Point(4.0, -1.0), Point(4.0, 1.0), Point(2.0, 4.0),
+        Point(-1.0, 4.0), Point(-4.0, 1.0), Point(-2.0, 0.0)
+    ),
+    listOf(
+        Point(1.0, 0.0), Point(4.0, 1.0), Point(4.0, 2.0),
+        Point(1.0, 4.0), Point(-2.0, 4.0), Point(-1.0, 2.0),
+        Point(-4.0, 2.0), Point(-4.0, -1.0), Point(-2.0, -4.0),
+        Point(1.0, -3.0), Point(2.0, -4.0), Point(4.0, -2.0),
+        Point(1.0, 0.0)
+    )
+)
+
 class AsteroidView {
-    private val rock = defineRocks().random()
+    private val rock = rocks.random()
 
     fun draw(asteroid: Asteroid, drawer: Drawer) {
         drawer.stroke = ColorRGBa.WHITE
@@ -19,67 +48,5 @@ class AsteroidView {
         drawer.strokeWeight = 8.0/30.0/sc
         drawer.scale(1.0, -1.0)
         drawer.lineStrip(rock)
-    }
-
-    private fun defineRocks(): List<List<Point>> {
-        val rock0 = listOf(
-            Point(4.0, 2.0),
-            Point(3.0, 0.0),
-            Point(4.0, -2.0),
-            Point(1.0, -4.0),
-            Point(-2.0, -4.0),
-            Point(-4.0, -2.0),
-            Point(-4.0, 2.0),
-            Point(-2.0, 4.0),
-            Point(0.0, 2.0),
-            Point(2.0, 4.0),
-            Point(4.0, 2.0),
-        )
-        val rock1 = listOf(
-            Point(2.0, 1.0),
-            Point(4.0, 2.0),
-            Point(2.0, 4.0),
-            Point(0.0, 3.0),
-            Point(-2.0, 4.0),
-            Point(-4.0, 2.0),
-            Point(-3.0, 0.0),
-            Point(-4.0, -2.0),
-            Point(-2.0, -4.0),
-            Point(-1.0, -3.0),
-            Point(2.0, -4.0),
-            Point(4.0, -1.0),
-            Point(2.0, 1.0)
-        )
-        val rock2 = listOf(
-            Point(-2.0, 0.0),
-            Point(-4.0, -1.0),
-            Point(-2.0, -4.0),
-            Point(0.0, -1.0),
-            Point(0.0, -4.0),
-            Point(2.0, -4.0),
-            Point(4.0, -1.0),
-            Point(4.0, 1.0),
-            Point(2.0, 4.0),
-            Point(-1.0, 4.0),
-            Point(-4.0, 1.0),
-            Point(-2.0, 0.0)
-        )
-
-        val rock3 = listOf(
-            Point(1.0, 0.0),
-            Point(4.0, 1.0),
-            Point(4.0, 2.0),
-            Point(1.0, 4.0),
-            Point(-2.0, 4.0),
-            Point(-1.0, 2.0),
-            Point(-4.0, 2.0),
-            Point(-4.0, -1.0),
-            Point(-2.0, -4.0),
-            Point(1.0, -3.0),
-            Point(2.0, -4.0),
-            Point(4.0, -2.0),
-            Point(1.0, 0.0)
-        )
-        return listOf(rock0,rock1,rock2,rock3)
     }
 }
