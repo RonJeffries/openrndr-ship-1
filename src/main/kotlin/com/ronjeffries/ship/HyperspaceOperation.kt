05122e778f1ee40871956bb6966a92cb0a508392
Ron Jeffries <ronjeffries@acm.org>
2022-11-30 16:02:29 -0500

    tidy lines

--- src/main/kotlin/com/ronjeffries/ship/HyperspaceOperation.kt
+++ src/main/kotlin/com/ronjeffries/ship/HyperspaceOperation.kt
@@ -4,9 +4,7 @@ import kotlin.random.Random
 
 class HyperspaceOperation(val ship: Ship, private val asteroidTally: Int) {
     fun execute(trans: Transaction) {
-        if (hyperspaceFails()) {
-            destroyTheShip(trans)
-        }
+        if (hyperspaceFails()) destroyTheShip(trans)
     }
 
     private fun destroyTheShip(trans: Transaction) {
@@ -16,12 +14,10 @@ class HyperspaceOperation(val ship: Ship, private val asteroidTally: Int) {
 
     private fun inHyperspace() = ship.position != U.CENTER_OF_UNIVERSE
 
-    private fun hyperspaceFails(): Boolean {
-        return inHyperspace() && hyperspaceFailure(Random.nextInt(0, 63), asteroidTally)
-    }
+    private fun hyperspaceFails(): Boolean
+        = inHyperspace() && hyperspaceFailure(Random.nextInt(0, 63), asteroidTally)
 
     // allegedly the original arcade rule
-    fun hyperspaceFailure(random0thru62: Int, asteroidTally: Int): Boolean {
-        return random0thru62 >= (asteroidTally + 44)
-    }
+    fun hyperspaceFailure(random0thru62: Int, asteroidTally: Int): Boolean
+        = random0thru62 >= (asteroidTally + 44)
 }
