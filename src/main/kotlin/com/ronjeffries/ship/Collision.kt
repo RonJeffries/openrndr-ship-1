854067f13d371d1bc1093175a527fddb4e5d2b77
Ron Jeffries <ronjeffries@acm.org>
2022-11-30 15:59:18 -0500

    tidy lines

--- src/main/kotlin/com/ronjeffries/ship/Collision.kt
+++ src/main/kotlin/com/ronjeffries/ship/Collision.kt
@@ -1,7 +1,6 @@
 package com.ronjeffries.ship
 
 class Collision(private val collider: Collider) {
-    fun hit(other: Collider): Boolean {
-        return collider.position.distanceTo(other.position) < collider.killRadius + other.killRadius
-    }
+    fun hit(other: Collider): Boolean
+        = collider.position.distanceTo(other.position) < collider.killRadius + other.killRadius
 }
