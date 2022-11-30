d7bebd92b72f3f71be6ae0176fe57266260f86a4
Ron Jeffries <ronjeffries@acm.org>
2022-11-30 09:32:55 -0500

    tidying Splat

--- src/main/kotlin/com/ronjeffries/ship/Splat.kt
+++ src/main/kotlin/com/ronjeffries/ship/Splat.kt
@@ -8,14 +8,13 @@ class Splat(
     var scale: Double = 1.0,
     var color: ColorRGBa = ColorRGBa.WHITE
 ) : ISpaceObject, InteractingSpaceObject {
-    
     constructor(ship: Ship) : this(ship.position, 2.0, ColorRGBa.WHITE)
     constructor(missile: Missile) : this(missile.position, 0.5, missile.color)
     constructor(saucer: Saucer) : this(saucer.position, 2.0, ColorRGBa.GREEN)
 
     var elapsedTime = 0.0
     private val lifetime = 2.0
-    private var view = SplatView(2.0)
+    private var view = SplatView(lifetime)
 
     override fun update(deltaTime: Double, trans: Transaction) {
         elapsedTime += deltaTime
