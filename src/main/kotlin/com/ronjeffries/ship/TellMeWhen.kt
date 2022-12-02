4b2c2e818953a3effff37468fa4eb2b67d74ddcf
Ron Jeffries <ronjeffries@acm.org>
2022-12-02 08:22:17 -0500

    TellmeWhen object implemented.

---
+++ src/main/kotlin/com/ronjeffries/ship/TellMeWhen.kt
@@ -0,0 +1,25 @@
+package com.ronjeffries.ship
+
+class TellMeWhen(
+    private val delay: Double,
+    initialTransaction: Transaction,
+    private val action: (Transaction) -> Unit
+) : ISpaceObject, InteractingSpaceObject {
+    var elapsedTime = 0.0
+
+    init {
+        elapsedTime = 0.0
+        initialTransaction.add(this)
+    }
+
+    override fun update(deltaTime: Double, trans: Transaction) {
+        elapsedTime += deltaTime
+        if (elapsedTime > delay ) {
+            action(trans)
+            trans.remove(this)
+        }
+    }
+
+    override val subscriptions: Subscriptions = Subscriptions()
+    override fun callOther(other: InteractingSpaceObject, trans: Transaction) {}
+}
---
+++ src/test/kotlin/com/ronjeffries/ship/TellMeWhenTest.kt
@@ -0,0 +1,23 @@
+package com.ronjeffries.ship
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class TellMeWhenTest {
+    var done = false
+    @Test
+    fun `triggers after n seconds`() {
+        val trans = Transaction()
+        TellMeWhen(2.0, trans) { _ -> done = true}
+        val tmw = trans.firstAdd()
+        val newTrans = Transaction()
+        tmw.update(1.1, newTrans)
+        assertThat(done).isEqualTo(false)
+        assertThat(newTrans.adds).isEmpty()
+        assertThat(newTrans.removes).isEmpty()
+        tmw.update(1.1, newTrans)
+        assertThat(done).isEqualTo(true)
+        assertThat(newTrans.adds).isEmpty()
+        assertThat(newTrans.removes).contains(tmw)
+    }
+}
