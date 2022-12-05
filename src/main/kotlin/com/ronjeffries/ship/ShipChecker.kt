285b8201c92e18bc4e4ff1dcb9dfe7a8747d88da
Ron Jeffries <ronjeffries@acm.org>
2022-12-05 09:11:09 -0500

    hot fix to bug where hyperspace cost player a ship every time.

--- src/main/kotlin/com/ronjeffries/ship/ShipChecker.kt
+++ src/main/kotlin/com/ronjeffries/ship/ShipChecker.kt
@@ -11,12 +11,14 @@ class ShipChecker(
         beforeInteractions = { missingShip = true },
         interactWithShip = { _, _ -> missingShip = false },
         afterInteractions = { trans ->
-            if ( missingShip && scoreKeeper.takeShip()) {
+            if ( missingShip && (inHyperspace() || scoreKeeper.takeShip())) {
                 trans.add(ShipMaker(ship, scoreKeeper))
                 trans.remove(this)
             }
         }
     )
+
+    private fun inHyperspace() = ship.position != U.CENTER_OF_UNIVERSE
 
     override fun callOther(other: InteractingSpaceObject, trans: Transaction) =
         other.subscriptions.interactWithShipChecker(this, trans)
