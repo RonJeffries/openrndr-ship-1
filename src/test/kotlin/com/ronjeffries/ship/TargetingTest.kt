9559b24fea84adddadc2dc1fa7c5ac9a568dc901
Ron Jeffries <ronjeffries@acm.org>
2022-12-19 09:52:37 -0500

    TargetingTest handles one coordinate.

---
+++ src/test/kotlin/com/ronjeffries/ship/TargetingTest.kt
@@ -0,0 +1,42 @@
+package com.ronjeffries.ship
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.math.abs
+
+fun preferred(shooter: Double, target: Double) : Double {
+    val lowerTarget = target - 1024
+    val higherTarget = target + 1024
+    val actualDistance = abs(target-shooter)
+    val lowerDistance = abs(lowerTarget - shooter)
+    val higherDistance = abs(higherTarget - shooter)
+    return when {
+        lowerDistance < actualDistance -> lowerTarget
+        higherDistance < actualDistance -> higherTarget
+        else -> target
+    }
+}
+
+fun preferred(shooter: Int, target: Int): Double {
+    return preferred(shooter.toDouble(), target.toDouble())
+}
+
+class TargetingTest {
+    @Test
+    fun `closest x coordinate`() {
+        val shooterX = 100.0
+        val targetX = 200.0
+        val preferredX = preferred(shooterX, targetX)
+        assertThat(preferredX).isEqualTo(200.0)
+    }
+
+    @Test
+    fun `left better`() {
+        assertThat(preferred(100, 900)).isEqualTo(-124.0)
+    }
+
+    @Test
+    fun `right better`() {
+        assertThat(preferred(1000, 100)).isEqualTo(1124.0)
+    }
+}
